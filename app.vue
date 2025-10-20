4fc1d30795cdc622dabf938d8f2d8475e0a394c7
06171002 <guri8376@gmail.com>
2025-10-20 15:13:36 +0900

    1020

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <NuxtLayout>
+      <h1 class="text-3xl font-bold text-blue-500 underline">
+        Hello Tailwind!
+      </h1>
       <NuxtPage />
     </NuxtLayout>
   </div>
