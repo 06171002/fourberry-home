5edfff42e3e6b10133110fcf6e8571038dac7a72
06171002 <guri8376@gmail.com>
2025-10-23 17:59:50 +0900

    1023

--- components/HomeSection/SectionSolutions.vue
+++ components/HomeSection/SectionSolutions.vue
@@ -9,30 +9,35 @@ const scrapingIconUrl = '/images/web_scraper.svg'
 
 <template>
   <BaseSection
-      class="panel h-dvh bg-white" text-color="#111827">
+      class="panel h-dvh bg-white font-roman-section" text-color="#111827">
   <div class="text-center h-full flex flex-col justify-center">
-    <h2 class="text-5xl md:text-7xl font-extrabold mb-6 section-title text-gray-900">자체 개발 솔루션</h2>
+    <h2 class="text-5xl md:text-7xl font-extrabold mb-6 section-title text-gray-900">SOLUTION PROGRAM</h2>
     <p class="text-lg md:text-xl text-gray-700 mb-10 md:mb-12 leading-relaxed section-description large">
       고객의 비즈니스에 새로운 가치를 더하는 포베리만의 기술력을 만나보세요.
     </p>
     <div class="flex flex-col items-center gap-6 md:gap-8 mb-10 md:mb-12 md:flex-row md:justify-center solution-cards">
       <div class="bg-white p-6 md:p-8  w-full max-w-[350px]  flex flex-col items-center text-center">
-        <img :src="ssoIconUrl" alt="SSO 아이콘" class="h-12 w-12 mb-4 text-primary">
+        <img :src="ssoIconUrl" alt="SSO 아이콘" class="h-24 w-24 mb-4 text-primary">
         <h3 class="text-xl md:text-2xl font-semibold mb-2 text-gray-800">베리패스 (Berry-Pass)</h3>
-        <p class="text-base md:text-lg text-gray-600 leading-normal">SSO 솔루션: 다양한 인증 통합 관리</p>
+<!--        <p class="text-base md:text-lg text-gray-600 leading-normal">SSO 솔루션: 다양한 인증 통합 관리</p>-->
       </div>
       <div class="bg-white p-6 md:p-8  w-full max-w-[350px]  flex flex-col items-center text-center">
-        <img :src="scrapingIconUrl" alt="웹 자동화 아이콘" class="h-12 w-12 mb-4 text-primary">
+        <img :src="scrapingIconUrl" alt="웹 자동화 아이콘" class="h-24 w-24 mb-4 text-primary">
         <h3 class="text-xl md:text-2xl font-semibold mb-2 text-gray-800">베리마인 (Berry-Mine)</h3>
-        <p class="text-base md:text-lg text-gray-600 leading-normal">웹 자동화 솔루션: 정보 수집 및 RPA</p>
+<!--        <p class="text-base md:text-lg text-gray-600 leading-normal">웹 자동화 솔루션: 정보 수집 및 RPA</p>-->
       </div>
     </div>
-    <NuxtLink to="/services" class="inline-block bg-transparent text-primary border-2 border-primary rounded-lg py-3 px-8 md:py-4 md:px-10 no-underline font-bold transition-all duration-200 ease-in-out hover:bg-primary hover:text-white hover:border-primary details-button">자세히 보기</NuxtLink>
+    <NuxtLink to="/services" class="inline-block text-primary border border-primary rounded-lg py-3 px-8 md:py-4 md:px-10 no-underline font-bold transition-all duration-200 ease-in-out hover:bg-blue-700 hover:scale-105 details-button">
+      SEE ALL PRODUCT
+    </NuxtLink>
   </div>
   </BaseSection>
 </template>
 
 <style scoped>
+.font-roman-section {
+  font-family: "Roman";
+}
 /* 카드 호버 효과는 유지 */
 .solution-cards .card {
   transition: transform 0.3s ease, box-shadow 0.3s ease;
